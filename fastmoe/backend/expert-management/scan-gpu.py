6ed33ead9b401464655a271c36caf58bb187fa64
Nikhil Gopal <nikhil2@gmail.com>
2025-08-10 19:51:33 -0600

    as

--- fastmoe/backend/expert-management/scan-gpu.py
+++ fastmoe/backend/expert-management/scan-gpu.py
@@ -4,8 +4,8 @@ import os
 
 num_gpus = torch.cuda.device_count()
 
-model_config_path = "fastmoe/models/mixtral-offload.py"
-model_config = ModelConfig(model_config_path)
+model_config_path = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+model_config = ModelConfig()
 assert os.path.exists(model_config_path), f"Model config file {model_config_path} does not exist"
 
 tp_size = model_config.tp_size
